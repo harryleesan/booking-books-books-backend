5044b78cd3601dc96d60afd4be99f85e5c9661a0
Harry Lee <harry@docdir.io>
2018-12-19 10:32:43 +0200

    Added MONGO_CONN as env var

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,10 +4,20 @@ import (
 	"booking-books-books-backend/pkg/mongo"
 	"booking-books-books-backend/pkg/server"
 	"log"
+	"os"
 )
 
 func main() {
-	ms, err := mongo.NewSession("mongodb://root:password@mongo:27017")
+	var mongoConn string = "mongodb://root:password@mongo:27017"
+
+	if m := os.Getenv("MONGO_CONN"); m != "" {
+		log.Print("MongoDB connection string found. Setting..")
+		mongoConn = m
+	} else {
+		log.Print("MongoDB connection string not set, using default.")
+	}
+
+	ms, err := mongo.NewSession(mongoConn)
 	if err != nil {
 		log.Fatalln("Unable to connect to mongodb")
 	}
